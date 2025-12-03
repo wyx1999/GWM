1a9607da5fafd28c53763c61178e5f0255097c80
Wei Yuxiang <59172411+ZJU-350@users.noreply.github.com>
2025-12-03 16:38:19 +0800

    Delete assembly.sh

--- assembly.sh
+++
@@ -1,9 +0,0 @@
-
-SAMPLE="sampleID"
-
-megahit \
-  -1 "/trimdata/${SAMPLE}/${SAMPLE}R1P.fq.gz" \
-  -2 "/trimdata/${SAMPLE}/${SAMPLE}R2P.fq.gz" \
-  -o "/megahit/${SAMPLE}" \
-  -t 28 \
-  --presets meta-large
